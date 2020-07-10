8d0edc24e5b2f5b6b4b6d28b2b743582ab6b037f
Ksbooth01 <67508242+Ksbooth01@users.noreply.github.com>
2020-07-10 09:57:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Download the latest Ghost Image
 resource "docker_image" "image_id" {
-  name = "${var.image_name}"
+  name = "ghost:latest"
 }
 
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = "${var.container_name}"
-  image = "${docker_image.image_id.latest}"
+  name  = "ghost_app"
+  image = docker_image.image_id.latest
   ports {
-    internal = "${var.int_port}"
-    external = "${var.ext_port}"
+    internal = "80"
+    external = "80"
   }
 }
